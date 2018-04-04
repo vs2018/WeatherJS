b353f73770ca977c2a1d62fab71992894f1cf7c3
Vishal Sharma <vsh1819@gmail.com>
2018-04-04 15:56:42 +0100

    created UI class and paint method

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA')
 
+// Init UI
+const ui = new UI()
+
 // Get weather on DOM load
 
 document.addEventListener('DOMContentLoaded', getWeather)
@@ -11,7 +14,7 @@ function getWeather(){
 
   weather.getWeather()
   .then(results => {
-    console.log(results)
+    ui.paint(results)
   })
   .catch(err => console.log(err))
 
