ad4d74ecc39b46dc4691473b0f64d53d65a05d06
Vishal Sharma <vsh1819@gmail.com>
2018-04-04 16:01:14 +0100

    added event listener for changing location

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ const ui = new UI()
 
 document.addEventListener('DOMContentLoaded', getWeather)
 
-// weather.changeLocation('Miami', 'FL')
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value
+  const state = document.getElementById('state').value
+
+  weather.changeLocation(city, state)
+
+  // Get and display weather
+  getWeather()
+
+  // Close modal
+  $('#locModal').modal('hide')
+
+
+})
+
 
 function getWeather(){
 
