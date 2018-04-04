1d7123a28942e420409039179a3c878adf853312
Vishal Sharma <vsh1819@gmail.com>
2018-04-04 15:32:21 +0100

    created html elements, weather class to fetch the data and an event listened to load default city and state API object on domcontentloaded

---
+++ weather.js
@@ -0,0 +1,25 @@
+class Weather {
+  constructor(city, state){
+    this.apiKey = 'f2d7612bd71090b2'
+    this.city = city
+    this.state = state
+  }
+
+  // Fetch weather from API
+  async getWeather () {
+    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`)
+
+    const responseData = await response.json()
+
+    return responseData.current_observation
+
+  }
+
+  // Change weather location
+
+  changeLocation(city, state){
+    this.city = city
+    this.state = state
+  }
+
+}
